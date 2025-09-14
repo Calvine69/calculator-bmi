5ee6982d0cef30553d9462851ee1abb3f7b60745
Calvine69 <Clvn.70@gmail.com>
2025-09-14 14:42:17 +0700

    update

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function calculateBMI() {
-  let weight = document.getElementById("weight").value;
-  let height = document.getElementById("height").value;
+  let berat = document.getElementById("berat").value;
+  let tinggi = document.getElementById("tinggi").value;
 
-  if (weight === "" || height === "" || weight <= 0 || height <= 0) {
-    alert("Harap masukkan data dengan benar!");
+  if (berat === "" || tinggi === "" || berat <= 0 || tinggi <= 0) {
+    alert("masukan data yang benar");
     return;
   }
 
-  height = height / 100;
+  tinggi = tinggi / 100;
 
-  let bmi = weight / (height * height);
+  let bmi = berat / (tinggi * tinggi);
   let kategori = "";
 
   if (bmi < 18.5) {
@@ -22,7 +22,6 @@ function calculateBMI() {
     kategori = "Obesitas";
   }
 
-  
   document.getElementById("bmiValue").innerText = bmi.toFixed(1);
   document.getElementById("bmikategori").innerText = kategori;
 }
